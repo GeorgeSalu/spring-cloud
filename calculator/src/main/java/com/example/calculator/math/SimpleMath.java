c04828760d1270d17bc46cead80fbf8f538133f3
GeorgeSalu <george.salu10@gmail.com>
2023-07-22 12:19:17 -0300

    start projeto calculator nivelamento - implementando as demas operacoes e refatorações

---
+++ calculator/src/main/java/com/example/calculator/math/SimpleMath.java
@@ -0,0 +1,21 @@
+package com.example.calculator.math;
+
+public class SimpleMath {
+
+	public Double sum(Double firstName,Double secondName) {
+		return firstName + secondName;
+	}
+	
+	public Double subtraction(Double firstName,Double secondName) {
+		return firstName - secondName;
+	}
+	
+	public Double multiplication(Double firstName,Double secondName) {
+		return firstName * secondName;
+	}
+	
+	public Double division(Double firstName,Double secondName) {
+		return firstName * secondName;
+	}
+	
+}
