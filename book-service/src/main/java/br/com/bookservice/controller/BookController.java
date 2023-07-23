33bbe27df045e4405f71b910224404375054275d
GeorgeSalu <george.salu10@gmail.com>
2023-07-23 18:13:29 -0300

    criando e conectando microservisos com feign - usando o feign rest cliente para invocar o cambio service

--- book-service/src/main/java/br/com/bookservice/controller/BookController.java
+++ book-service/src/main/java/br/com/bookservice/controller/BookController.java
@@ -1,18 +1,15 @@
 package br.com.bookservice.controller;
 
-import java.util.HashMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import br.com.bookservice.model.Book;
+import br.com.bookservice.proxy.CambioProxy;
 import br.com.bookservice.repository.BookRepository;
-import br.com.bookservice.response.Cambio;
 
 @RestController
 @RequestMapping("book-service")
@@ -24,23 +21,19 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 	
+	@Autowired
+	private CambioProxy cambioProxy;
+	
 	@GetMapping(value = "/{id}/{currency}")
 	public Book findBook(@PathVariable("id")Long id,@PathVariable("currency")String currency) {
 		var book = repository.getById(id);
 		if(book == null) throw new RuntimeException("Book not found");
 		
-		HashMap<String, String> params = new HashMap<>();
-		params.put("amount", book.getPrice().toString());
-		params.put("from", "USD");
-		params.put("to", currency);
 		
-		var response = new RestTemplate()
-			.getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}", Cambio.class, params);
-		
-		var cambio = response.getBody();
+		var cambio = cambioProxy.getCambio(book.getPrice(), "USD", currency);
 		
 		var port = environment.getProperty("local.server.port");
-		book.setEnviroment(port);
+		book.setEnviroment(port+" feign");
 		book.setPrice(cambio.getConvertedvalue());
 		return book;
 	}
