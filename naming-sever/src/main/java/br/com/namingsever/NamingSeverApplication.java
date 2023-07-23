e0470b06615205ab63c59d98b8317f638e36c00e
GeorgeSalu <george.salu10@gmail.com>
2023-07-23 18:26:48 -0300

    service discovery e service resgistry com eureka - criação do microservice naming-service

---
+++ naming-sever/src/main/java/br/com/namingsever/NamingSeverApplication.java
@@ -0,0 +1,13 @@
+package br.com.namingsever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingSeverApplication.class, args);
+	}
+
+}
