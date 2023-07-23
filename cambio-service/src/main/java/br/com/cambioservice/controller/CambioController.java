b0a2c8d5f96266f4e0a6421322a8b111b843eeec
GeorgeSalu <george.salu10@gmail.com>
2023-07-23 14:34:32 -0300

    criando e conectando microservisos com feing - recuperando a porta da instancia em execução

--- cambio-service/src/main/java/br/com/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/br/com/cambioservice/controller/CambioController.java
@@ -2,6 +2,8 @@ package br.com.cambioservice.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,16 @@ import br.com.cambioservice.model.Cambio;
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
+	
+	@Autowired
+	private Environment environment;
 
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	private Cambio getCambio(@PathVariable("amount") BigDecimal amout,@PathVariable("from")String from,@PathVariable("to")String to) {
-		return new Cambio(1L, from, to , BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+		
+		var port = environment.getProperty("local.server.port");
+		
+		return new Cambio(1L, from, to , BigDecimal.ONE, BigDecimal.ONE, port);
 	}
 	
 }
