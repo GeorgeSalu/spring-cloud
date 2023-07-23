c5d627adc2f7810355e55d08db5cd7e51d8b1308
GeorgeSalu <george.salu10@gmail.com>
2023-07-23 10:50:04 -0300

    spring cloud configuration - recuperando os valores da confirguracao

--- greeting-service/src/main/java/br/com/greetingservice/controller/GreetingController.java
+++ greeting-service/src/main/java/br/com/greetingservice/controller/GreetingController.java
@@ -2,21 +2,27 @@ package br.com.greetingservice.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.greetingservice.configuration.GreetingConfiguration;
 import br.com.greetingservice.model.Greeting;
 
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s";
+	private static final String template = "%s, %s";
 	private final AtomicLong counter = new AtomicLong();
+	
+	@Autowired
+	private GreetingConfiguration configuration;
 	
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		if(name.isEmpty()) name = configuration.getDefaultValue();
+		return new Greeting(counter.incrementAndGet(), String.format(template, configuration.getGreeting(), name));
 	}
 	
 }
